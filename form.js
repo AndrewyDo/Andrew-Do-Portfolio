eb9eb5931fda04d222133a26181fcbaf82c9d08a
andrewdo <ado3@luc.edu>
2024-04-14 17:02:07 -0500

    Update - Contact page(link to google sheets for data management) and Education (coursework)

---
+++ form.js
@@ -0,0 +1,16 @@
+const scriptURL = 'https://script.google.com/macros/s/AKfycbxXb5NCj_j7b_dT9NvvP9RMgeR8aAAS0gp_dzncht6f70344Lm4EOk1OhA3vYMnkMdqMQ/exec'
+const form = document.forms['submit-to-google-sheet']
+const message = document.getElementById("message")
+
+form.addEventListener('submit', e => {
+e.preventDefault()
+fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+    .then(response => {
+        message.innerHTML = "Message sent successfully!"
+        setTimeout(function() {
+            message.innerHTML = ""
+        }, 3000)
+        form.reset()
+    })
+    .catch(error => console.error('Error!', error.message))
+})
